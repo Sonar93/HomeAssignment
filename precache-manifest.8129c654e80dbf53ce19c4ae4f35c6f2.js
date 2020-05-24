1d8b200438409dc788d17899ffab56122da34cd8
Sonar93 <sonar.sumit93@gmail.com>
2020-05-24 20:52:00 +0530

    Updates

---
+++ precache-manifest.8129c654e80dbf53ce19c4ae4f35c6f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e7d1a935622b234bd0e1d2fbc7e222",
+    "url": "/HomeAssignment/index.html"
+  },
+  {
+    "revision": "b0826ee062b6360c9409",
+    "url": "/HomeAssignment/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e7ebe5c7f15ce4e07883",
+    "url": "/HomeAssignment/static/css/main.6dd7b4dd.chunk.css"
+  },
+  {
+    "revision": "b0826ee062b6360c9409",
+    "url": "/HomeAssignment/static/js/2.6d55c2f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeAssignment/static/js/2.6d55c2f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ebe5c7f15ce4e07883",
+    "url": "/HomeAssignment/static/js/main.b8535fa4.chunk.js"
+  },
+  {
+    "revision": "9ef507e50fe5661ace1e",
+    "url": "/HomeAssignment/static/js/runtime-main.d98e8f11.js"
+  }
+]);
